b5294e6c33170b7874ccd426438a4c7a073eb718
Louise Deeth <louisedeeth@gmail.com>
2024-11-18 15:58:26 +0000

    Added plot multiple images

--- Lab8/Lab8.py
+++ Lab8/Lab8.py
@@ -2,8 +2,23 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+nrows = 2
+ncols = 1
+
 img = cv2.imread('ATU.jpg',)
 
+#single images
 gray_image = cv2. cvtColor(img, cv2. COLOR_BGR2GRAY)
-cv2. imshow('Original Image', img) cv2. imshow('Grayscale Image', gray_image)
-cv2. waitKey(0) cv2. destroyAllWindows()
+cv2. imshow('Original Image', img) 
+cv2. imshow('Grayscale Image', gray_image)
+cv2. waitKey(0) 
+cv2. destroyAllWindows()
+
+#multiple images
+plt.subplot(nrows, ncols,1),plt.imshow(imgOrig, cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,2),plt.imshow(imgGray, cmap = 'gray')
+plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
+plt.show()
+
+imgOut = cv2.GaussianBlur(imgIn,(KernelSizeWidth, KernelSizeHeight),0)
