1c0e9d181763a3d8dad10ad3c75b4456f5ce575c
Louise Deeth <louisedeeth@gmail.com>
2024-11-18 15:36:02 +0000

    First commit

---
+++ Lab8/Lab8.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('ATU.jpg',)
+
+gray_image = cv2. cvtColor(img, cv2. COLOR_BGR2GRAY)
+cv2. imshow('Original Image', img) cv2. imshow('Grayscale Image', gray_image)
+cv2. waitKey(0) cv2. destroyAllWindows()
