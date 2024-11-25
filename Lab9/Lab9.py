c679d1e1c49931d5c1cceabc9a87e58e7bcb430b
Louise Deeth <louisedeeth@gmail.com>
2024-11-25 16:54:37 +0000

    Added Harris Corners circles

--- Lab9/Lab9.py
+++ Lab9/Lab9.py
@@ -5,6 +5,9 @@ from matplotlib import pyplot as plt
 
 nrows = 3
 ncols = 3
+B = 0
+G = 0
+R = 255
 
 img = cv2.imread('ATU1.jpg',)
 
@@ -12,12 +15,22 @@ gray_image = cv2. cvtColor(img, cv2. COLOR_BGR2GRAY)
 cv2. waitKey(0) 
 cv2. destroyAllWindows()
 
-dst = cv2.cornerHarris(img, 2, 3, 0.04)
-imgHarris = copy.deepcopy(dst) 
+dst = cv2.cornerHarris(gray_image, 2, 3, 0.04) #corner harris of grayscale img
+imgHarris = copy.deepcopy(img) #deep copy of the original img
  
-threshold = 0.9; #number between 0 and 1
+# To plot the detected Harris corners, loop through every element in the 2d matrix –
+# dst. If the element is greater than a threshold, draw a circle on the image as follows
+threshold = 0.04; #number between 0 and 1
 for i in range(len(dst)):
     for j in range(len(dst[i])):
         if dst[i][j] > (threshold*dst.max()):
-            cv2.circle(imgHarris,(j,i),3,(255, 243, 0),-1)
+            cv2.circle(imgHarris,(j,i),3,(B, G, R),-1)
 
+#plot multiple images
+plt.subplot(nrows, ncols,1),plt.imshow(cv2. cvtColor(img, cv2. COLOR_BGR2RGB), cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,2),plt.imshow(gray_image, cmap = 'gray')
+plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,3),plt.imshow(cv2. cvtColor(imgHarris, cv2. COLOR_BGR2RGB), cmap = 'gray')
+plt.title('Harris Corners'), plt.xticks([]), plt.yticks([])
+plt.show()
