96a8a37a633aafaa34b55886f1774332cf91ac81
Louise Deeth <louisedeeth@gmail.com>
2024-11-25 16:23:54 +0000

    Added threshold code

--- Lab9/Lab9.py
+++ Lab9/Lab9.py
@@ -13,3 +13,11 @@ cv2. waitKey(0)
 cv2. destroyAllWindows()
 
 dst = cv2.cornerHarris(img, 2, 3, 0.04)
+imgHarris = copy.deepcopy(dst) 
+ 
+threshold = 0.9; #number between 0 and 1
+for i in range(len(dst)):
+    for j in range(len(dst[i])):
+        if dst[i][j] > (threshold*dst.max()):
+            cv2.circle(imgHarris,(j,i),3,(255, 243, 0),-1)
+
